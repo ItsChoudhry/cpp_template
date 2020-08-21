a64bd9aa66c9ec6cb13433428312c3b016d9d484
itsChoudhry <23042676+ItsChoudhry@users.noreply.github.com>
2020-08-21 19:10:32 +0100

    Update cmake stuff

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,18 +1,8 @@
 #include <catch2/catch.hpp>
 #include "thing.hpp"
 
-unsigned int Factorial(unsigned int number) {
-  return number <= 1 ? number : Factorial(number - 1) * number;
-}
-
-TEST_CASE("Factorials are computed", "[factorial]") {
-  REQUIRE(Factorial(1) == 1);
-  REQUIRE(Factorial(2) == 2);
-  REQUIRE(Factorial(3) == 6);
-  REQUIRE(Factorial(10) == 3628800);
-}
-
-TEST_CASE("thing is squared", "[thing]") {
+TEST_CASE("thing is squared", "[thing]")
+{
   REQUIRE(thing(5) == 25);
   REQUIRE(thing(10) == 100);
   REQUIRE(thing(2) == 4);
